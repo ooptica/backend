ca627076bc3bf29945445dadd57fc9d1a7e90e28
AhmetAlperYilmaz <alperyilmaz2000@hotmail.com>
2019-05-19 17:41:07 +0300

    Changed backend

--- server.py
+++ server.py
@@ -2,69 +2,22 @@
 # Imports
 #----------------------------------------------------------------------------#
 
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request, abort, Response
 import os
-from io import BytesIO
-import numpy as np
-import cv2
-from helpers import getFeatures, drawFeatures
-import base64
-from PIL import Image
-from keras.models import model_from_json
-import tensorflow as tf
-global graph,model
-graph = tf.get_default_graph()
 
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
 app.config.from_object('config')
 
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-model = model_from_json(loaded_model_json)
-# load weights into new model
-model.load_weights("model.h5")
-
-@app.route('/landmarks', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def home():
-    arr = None
-    if "image" in request.form:
-        img_str = request.form['image']
-        img_decoded = base64.b64decode(img_str)
-        img_file = Image.open(BytesIO(img_decoded))
-        arr = np.asarray(img_file, np.uint8)
-
-    if arr is not None:
-        resulter = getFeatures(arr)
-        if len(resulter) > 0:
-            normalizeddots=[]
-            (bbox,dots)=resulter[0]
-            (faceX, faceY, faceW, faceH) = bbox
-            for (x,y) in dots:
-                normalizedX = (x - faceX) / faceW
-                normalizedY = (y - faceY) / faceH
-                normalizeddots.append(normalizedX)
-                normalizeddots.append(normalizedY)
-            if len(normalizeddots) == 136:
-                img_show = cv2.resize(cv2.cvtColor(arr, cv2.COLOR_RGB2BGR),(int(480),int(640)))
-
-                with graph.as_default():
-                    prediction=model.predict(np.array([normalizeddots]))
-
-                flat_list = [int(item*100) for sublist in prediction for item in sublist]
-                print(flat_list)
-
-                res = {
-                    "category" : flat_list.index(max(flat_list)),
-                    "confidence" : max(flat_list)
-                }
-                return jsonify(category = flat_list.index(max(flat_list)), confidence = max(flat_list))
-    else:
-        print('err')
-        return 'Error'
+    return app.send_static_file("index.html")
+    
+@app.errorhandler(404)
+def not_found_error(error):
+    return app.send_static_file("404.html"), 404
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
